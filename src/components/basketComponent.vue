f628f28ce2c1da8e7c528556bc94471c07798ae1
TriziMMA <neyag90707@galcake.com>
2023-03-25 20:46:18 +0300

    UPDATE version 1.0.1, change v-card

--- src/components/basketComponent.vue
+++ src/components/basketComponent.vue
@@ -130,80 +130,82 @@ console.log(cordlessDrillArray);
             </div>
           </v-card>
 
+<!--          Ниже изначальный вариант v-card. START-->
+        <!--        <v-card-->
+        <!--            width="750"-->
+        <!--            height="250"-->
+        <!--            color="background"-->
+        <!--            elevation="5"-->
+        <!--            class="pa-1 ma-1 d-flex justify-space-around"-->
+        <!--            v-for="i in example2">-->
+        <!--          <v-row>-->
+        <!--            &lt;!&ndash;      FIRST COL&ndash;&gt;-->
+        <!--            <v-col cols="4"-->
+        <!--                   class="pa-1">-->
+        <!--              &lt;!&ndash;    CARD ITEM START&ndash;&gt;-->
+        <!--              <v-card-item-->
+        <!--                  class="pa-1">-->
+        <!--                <v-card-subtitle>код: {{ i.id }}<br> {{ i.typeThis }}</v-card-subtitle>-->
+        <!--                <div class="photoInCard">-->
+        <!--                  <v-img-->
+        <!--                      width="100%"-->
+        <!--                      :src="i.imgTitle"></v-img>-->
+        <!--                </div>-->
+        <!--              </v-card-item>-->
+        <!--              &lt;!&ndash;    CARD ITEM END&ndash;&gt;-->
+        <!--            </v-col>-->
+        <!--            &lt;!&ndash;      SECOND COL&ndash;&gt;-->
+        <!--            <v-col cols="5"-->
+        <!--                   class="pa-1">-->
+        <!--              &lt;!&ndash;        TITLE&ndash;&gt;-->
+        <!--              <div class="blockTitleCard">-->
+        <!--                <button class="cardTextHref">{{ i.name }}</button>-->
+        <!--              </div>-->
+        <!--              &lt;!&ndash;        SPAN AND TEXT&ndash;&gt;-->
+        <!--              <div-->
+        <!--                  class="mt-1">-->
+        <!--                <v-card-text v-for="item in example2[i.numberInList].featureTopTitle"-->
+        <!--                             key="item"-->
+        <!--                             class="textCardFeature pa-0">{{ item.featureTopTitleInfoTitle }}-->
+        <!--                  <span class="spanTextCard">{{ item.featureTopTitleInfoText }}</span></v-card-text>-->
+        <!--              </div>-->
 
+
+        <!--            </v-col>-->
+        <!--            &lt;!&ndash;      THIRD COL&ndash;&gt;-->
+        <!--            <v-col cols="3"-->
+        <!--                   class="pa-1">-->
+        <!--              &lt;!&ndash;    CARD ACTIONS START&ndash;&gt;-->
+        <!--              <p class="textCardPrice pa-2">-->
+        <!--                {{ i.price }} рублей-->
+        <!--              </p>-->
+        <!--              <v-card-actions-->
+        <!--                  class="pa-0 pr-1">-->
+
+        <!--                <v-btn-->
+        <!--                    @click=""-->
+        <!--                    elevation="1"-->
+        <!--                    class="vBtnBuy"-->
+        <!--                    width="170px"-->
+        <!--                    prepend-icon="fa-solid fa-cart-shopping"-->
+        <!--                >-->
+        <!--                  Убрать с корзины-->
+        <!--                </v-btn>-->
+        <!--              </v-card-actions>-->
+        <!--              <p class="textCardAvailability">-->
+        <!--                В наличии имеется > {{ i.availability }}-->
+        <!--              </p>-->
+        <!--              &lt;!&ndash;    CARD ACTIONS END&ndash;&gt;-->
+        <!--            </v-col>-->
+        <!--          </v-row>-->
+        <!--        </v-card>-->
+<!--              END-->
         <p class="example">{{ selected }}</p>
       </div>
       <div class="blockSecondBasket"></div>
     </div>
 
-    <!--        <v-card-->
-    <!--            width="750"-->
-    <!--            height="250"-->
-    <!--            color="background"-->
-    <!--            elevation="5"-->
-    <!--            class="pa-1 ma-1 d-flex justify-space-around"-->
-    <!--            v-for="i in example2">-->
-    <!--          <v-row>-->
-    <!--            &lt;!&ndash;      FIRST COL&ndash;&gt;-->
-    <!--            <v-col cols="4"-->
-    <!--                   class="pa-1">-->
-    <!--              &lt;!&ndash;    CARD ITEM START&ndash;&gt;-->
-    <!--              <v-card-item-->
-    <!--                  class="pa-1">-->
-    <!--                <v-card-subtitle>код: {{ i.id }}<br> {{ i.typeThis }}</v-card-subtitle>-->
-    <!--                <div class="photoInCard">-->
-    <!--                  <v-img-->
-    <!--                      width="100%"-->
-    <!--                      :src="i.imgTitle"></v-img>-->
-    <!--                </div>-->
-    <!--              </v-card-item>-->
-    <!--              &lt;!&ndash;    CARD ITEM END&ndash;&gt;-->
-    <!--            </v-col>-->
-    <!--            &lt;!&ndash;      SECOND COL&ndash;&gt;-->
-    <!--            <v-col cols="5"-->
-    <!--                   class="pa-1">-->
-    <!--              &lt;!&ndash;        TITLE&ndash;&gt;-->
-    <!--              <div class="blockTitleCard">-->
-    <!--                <button class="cardTextHref">{{ i.name }}</button>-->
-    <!--              </div>-->
-    <!--              &lt;!&ndash;        SPAN AND TEXT&ndash;&gt;-->
-    <!--              <div-->
-    <!--                  class="mt-1">-->
-    <!--                <v-card-text v-for="item in example2[i.numberInList].featureTopTitle"-->
-    <!--                             key="item"-->
-    <!--                             class="textCardFeature pa-0">{{ item.featureTopTitleInfoTitle }}-->
-    <!--                  <span class="spanTextCard">{{ item.featureTopTitleInfoText }}</span></v-card-text>-->
-    <!--              </div>-->
 
-
-    <!--            </v-col>-->
-    <!--            &lt;!&ndash;      THIRD COL&ndash;&gt;-->
-    <!--            <v-col cols="3"-->
-    <!--                   class="pa-1">-->
-    <!--              &lt;!&ndash;    CARD ACTIONS START&ndash;&gt;-->
-    <!--              <p class="textCardPrice pa-2">-->
-    <!--                {{ i.price }} рублей-->
-    <!--              </p>-->
-    <!--              <v-card-actions-->
-    <!--                  class="pa-0 pr-1">-->
-
-    <!--                <v-btn-->
-    <!--                    @click=""-->
-    <!--                    elevation="1"-->
-    <!--                    class="vBtnBuy"-->
-    <!--                    width="170px"-->
-    <!--                    prepend-icon="fa-solid fa-cart-shopping"-->
-    <!--                >-->
-    <!--                  Убрать с корзины-->
-    <!--                </v-btn>-->
-    <!--              </v-card-actions>-->
-    <!--              <p class="textCardAvailability">-->
-    <!--                В наличии имеется > {{ i.availability }}-->
-    <!--              </p>-->
-    <!--              &lt;!&ndash;    CARD ACTIONS END&ndash;&gt;-->
-    <!--            </v-col>-->
-    <!--          </v-row>-->
-    <!--        </v-card>-->
 
   </v-container>
 </template>
@@ -307,69 +309,69 @@ console.log(cordlessDrillArray);
 //
 
 
-//.v-card-title, .v-card-subtitle, .v-card-text {
-//  font-size: 0.8rem;
-//}
-//
-//.v-card {
-//
-//}
-//
-//.v-btn {
-//  color: $primary;
-//  background-color: $background;
-//}
-//
-//.photoInCard {
-//  width: 242px;
-//  height: 190px;
-//}
-//
-//.cardTextHref {
-//  font-size: 1.1rem;
-//  text-decoration: none;
-//  text-align: center;
-//  color: $textSpan;
-//  font-weight: 500;
-//}
-//
-//.blockTitleCard {
-//  width: 100%;
-//  display: flex;
-//  justify-content: center;
-//}
-//
-//.textCardPrice {
-//  font-size: 0.9rem;
-//  text-align: right;
-//  font-weight: 600;
-//  color: #282828;
-//}
-//
-//.spanTextCard {
-//  color: $textSpan;
-//}
-//
-//.v-card-text {
-//  color: $text;
-//}
-//
-//.textCardAvailability {
-//  color: $success;
-//  font-size: 0.8rem;
-//  font-weight: 600;
-//}
-//
-//.vBtnBuy {
-//  color: $background;
-//  background: $primary;
-//}
-//
-//.vBtnBuy:hover {
-//  color: $primary;
-//  background: $background;
-//  border: 1px solid $primary;
-//}
+.v-card-title, .v-card-subtitle, .v-card-text {
+  font-size: 0.8rem;
+}
+
+.v-card {
+
+}
+
+.v-btn {
+  color: $primary;
+  background-color: $background;
+}
+
+.photoInCard {
+  width: 242px;
+  height: 190px;
+}
+
+.cardTextHref {
+  font-size: 1.1rem;
+  text-decoration: none;
+  text-align: center;
+  color: $textSpan;
+  font-weight: 500;
+}
+
+.blockTitleCard {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.textCardPrice {
+  font-size: 0.9rem;
+  text-align: right;
+  font-weight: 600;
+  color: #282828;
+}
+
+.spanTextCard {
+  color: $textSpan;
+}
+
+.v-card-text {
+  color: $text;
+}
+
+.textCardAvailability {
+  color: $success;
+  font-size: 0.8rem;
+  font-weight: 600;
+}
+
+.vBtnBuy {
+  color: $background;
+  background: $primary;
+}
+
+.vBtnBuy:hover {
+  color: $primary;
+  background: $background;
+  border: 1px solid $primary;
+}
 
 
 </style>
