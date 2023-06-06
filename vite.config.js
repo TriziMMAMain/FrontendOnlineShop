9a9de3de8e41cf32424c1ee7177b14ef44905dc7
unknown <samparizonarp131212@gmail.com>
2023-06-06 21:07:24 +0300

    upd 21:07

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createFilter } from 'rollup-pluginutils';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
