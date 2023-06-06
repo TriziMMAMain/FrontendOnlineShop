c93e060c2d679eced433796f1cbda68bcfad2913
unknown <samparizonarp131212@gmail.com>
2023-06-06 21:41:45 +0300

    upd 21:41

--- src/router/index.js
+++ src/router/index.js
@@ -338,7 +338,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(process.env.NODE_ENV === 'production' ? 'frontend-online-shop-drab.vercel.app' : process.env.BASE_URL),
     routes
 })
 export default router
