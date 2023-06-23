169e9e452b6b6e61e5a91831c1ef7d8d6f864a3e
TriziMMAMain <samparizonarp131212@gmail.com>
2023-06-23 18:05:03 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const interceptors = axios.create({
-    // baseURL: 'https://backend-online-shop-mongodb.onrender.com/',
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'https://backend-online-shop-mongodb.onrender.com/',
+    // baseURL: 'http://localhost:4000/',
 });
 
 export default interceptors;
