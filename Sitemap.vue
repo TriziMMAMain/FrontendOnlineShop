514a816ee13c5d36031c67cb6396e315899a80ad
TriziMMAMain <samparizonarp131212@gmail.com>
2023-08-06 15:40:54 +0300

    upd

--- Sitemap.vue
+++ Sitemap.vue
@@ -2,19 +2,28 @@
   <pre>{{ xmlContent }}</pre>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue';
-import sitemap from './sitemap.xml'
-import axios from 'axios';
-const xmlContent = ref('');
+<script>
+import { ref } from 'vue';
 
-onMounted(async () => {
-  try {
-    const response = await axios.get('/sitemap.xml');
-    xmlContent.value = response.data;
-    console.log(xmlContent.value);
-  } catch (error) {
-    console.error(error);
-  }
-});
+export default {
+  data() {
+    return {
+      xmlContent: '',
+    };
+  },
+  mounted() {
+    this.loadXML();
+  },
+  methods: {
+    async loadXML() {
+      try {
+        const response = await fetch('/sitemap.xml');
+        const xmlData = await response.text();
+        this.xmlContent = xmlData;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
